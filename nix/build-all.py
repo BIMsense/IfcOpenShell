2cd035b2c70e34bb102de095dd0fc095026db14a
Ian Yeo <37275312+iyeoda@users.noreply.github.com>
2018-11-04 10:52:35 +0000

    Added manual cmake path

--- nix/build-all.py
+++ nix/build-all.py
@@ -137,7 +137,7 @@ try:
 except KeyError:
     TARGET_ARCH = sp.check_output([uname, "-m"]).strip()
 
-CMAKE_DIR=os.path.realpath(os.path.join("..", "cmake"))
+CMAKE_DIR=os.path.realpath(os.path.join("usr/tmp/ifcopenshell", "cmake"))
 
 try:
     DEPS_DIR = os.environ["DEPS_DIR"]
